fe75e1fde4546d08a0f02b5c2033e0bb6cc47813
Анатолий <anatolij@MacBook-Air-Anatolij.local>
2026-02-06 05:10:49 +0300

    Create utils dir + texts.py

--- handlers/about.py
+++ handlers/about.py
@@ -7,3 +7,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 info_router = Router()
 
 
+f'''
+Чтобы не захламлять хендлеры, то выносим текст о направлениях в отдельный файл texts.py
+в папку utils
+'''
